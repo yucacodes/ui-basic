bab8c6b5fa6d6a29eb89f13048c6c95d67427f2b
Jorge Narvaez <jormelcn@gmail.com>
2023-12-31 15:22:02 -0500

    Create Button Example

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,32 @@
-import { useState } from 'react'
+import SyntaxHighlighter from 'react-syntax-highlighter'
+import { atomOneLight, vs } from 'react-syntax-highlighter/dist/esm/styles/hljs'
 import './App.css'
-import { Button } from '@lib'
+
+import examples from './Test.example'
+import exampleSrc from './Test.example.tsx?raw'
+
+function prepareExampleSrc(src: string): string {
+  src = src.replaceAll('@lib', '@yucacodes/ui')
+  return src
+    .split('\n')
+    .filter((x) => !x.includes('__HIDE__'))
+    .join('\n')
+    .replaceAll('@lib', '@yucacodes/ui')
+}
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  const onClick = () => setCount((count) => count + 1)
   return (
     <>
-      <h1>Count</h1>
-      <p>{count}</p>
-      <p>
-        Presionas este &nbsp;
-        <Button onClick={onClick}>
-          botón  
-        </Button>
-        &nbsp; para contar
-      </p>
+      <h1>Source</h1>
+      <SyntaxHighlighter
+        customStyle={{ textAlign: 'left' }}
+        language="typescript"
+        style={vs}
+      >
+        {prepareExampleSrc(exampleSrc)}
+      </SyntaxHighlighter>
+      <h2>Preview</h2>
+      <div>{examples}</div>
     </>
   )
 }

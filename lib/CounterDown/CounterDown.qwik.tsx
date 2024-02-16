d081d59e89003a1ad5166f58a7d1248698acea77
Ricardo Bermudez Tellez <39892056+ricardober93@users.noreply.github.com>
2024-02-16 11:44:04 -0500

    se agregar una tag para iniciar el contador (#6)
    Co-authored-by: Ricardo Bermudez <ribermudez@petalmail.com>

--- lib/CounterDown/CounterDown.qwik.tsx
+++ lib/CounterDown/CounterDown.qwik.tsx
@@ -18,9 +18,10 @@ export interface CounterDownProps extends CCounterDownProps {
   class?: ClassList
   style?: CSSProperties
   beerTime?: number
+  started?: boolean
 }
 
-export const CounterDown = component$(({ beerTime, ...props }: CounterDownProps) => {
+export const CounterDown = component$(({ beerTime, started = true, ...props }: CounterDownProps) => {
 
   const variant = selectedFlag(props, counterVariantFlagsOptions)
 
@@ -32,6 +33,7 @@ export const CounterDown = component$(({ beerTime, ...props }: CounterDownProps)
   const hours = useSignal(0)
   const minutes = useSignal(0)
   const seconds = useSignal(0)
+  const interval = useSignal<any>()
 
   const distance = useSignal(0)
   useVisibleTask$(({ track }) => {
@@ -41,30 +43,32 @@ export const CounterDown = component$(({ beerTime, ...props }: CounterDownProps)
     track(() => seconds.value)
     track(() => now.value)
 
-    const interval = setInterval(() => {
-      now.value = new Date().getTime()
-      distance.value = beerTimed.value - now.value
-
-      days.value = Math.floor(distance.value / (1000 * 60 * 60 * 24))
-      hours.value = Math.floor(
-        (distance.value % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-      )
-      minutes.value = Math.floor((distance.value % (1000 * 60 * 60)) / (1000 * 60))
-      seconds.value = Math.floor((distance.value % (1000 * 60)) / 1000)
-
-      if (distance.value < 0) {
-        clearInterval(interval)
-        days.value = 0
-        hours.value = 0
-        minutes.value = 0
-        seconds.value = 0
-        
-      }
-     
-    }, 1000)
+    if (started) {
+       interval.value = setInterval(() => {
+        now.value = new Date().getTime()
+        distance.value = beerTimed.value - now.value
+  
+        days.value = Math.floor(distance.value / (1000 * 60 * 60 * 24))
+        hours.value = Math.floor(
+          (distance.value % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+        )
+        minutes.value = Math.floor((distance.value % (1000 * 60 * 60)) / (1000 * 60))
+        seconds.value = Math.floor((distance.value % (1000 * 60)) / 1000)
+  
+        if (distance.value < 0) {
+          clearInterval(interval.value)
+          days.value = 0
+          hours.value = 0
+          minutes.value = 0
+          seconds.value = 0
+          
+        }
+       
+      }, 1000)
+    }
 
    
-    return () => clearInterval(interval)
+    return () => clearInterval(interval.value)
   })
 
   if (variant === 'counterdown') {
--- src/docs/CounterDownDocs/CounterDown.example.qwik.tsx
+++ src/docs/CounterDownDocs/CounterDown.example.qwik.tsx
@@ -1,20 +1,12 @@
 import { Container, CounterDown} from '@yucacodes/ui-qwik'; // __EXCLUDE__
 // __EXCLUDE__
 export default ( // __EXCLUDE__
-  <Container relative fullSize
-    style={{
-      display: 'flex',
-      gap: '1rem',
-      alignItems: "center",
-      justifyContent: "center",
-      flexDirection: "column"
-    }}> {/*__EXCLUDE__*/}
+  <Container relative fullSize style={{ display: 'flex', gap: '1rem', alignItems: "center", justifyContent: "center", flexDirection: "column"}}> {/*__EXCLUDE__*/}
     <CounterDown />
-
 
     <CounterDown clock beerTime={new Date().getTime() + 1000 * 60 * 60 }/>
       
-    <CounterDown full />
+    <CounterDown full  started={false}/>
    
 
   </Container> // __EXCLUDE__

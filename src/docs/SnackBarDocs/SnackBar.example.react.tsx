7ea321819f447e1a4324596f14b1a8eff669f97f
SharonFlorez <shdflorezs35@gmail.com>
2024-04-25 21:14:22 -0500

    Remove code lines

--- src/docs/SnackBarDocs/SnackBar.example.react.tsx
+++ src/docs/SnackBarDocs/SnackBar.example.react.tsx
@@ -12,13 +12,5 @@ export default (
       width: '100%',
       gap: '1rem',
     }}
-  >
-    {/*__EXCLUDE__*/}
-    <>
-      {/*__EXCLUDE__*/}
-      <SnackBar message="Hola" textAction="Cerrar"></SnackBar>
-      <SnackBar secondary message="Mensaje 2" textAction="Click"></SnackBar>
-      <SnackBar danger message="Mensaje 3" textAction="Ok"></SnackBar>
-    </>
-  </Container> // __EXCLUDE__
+  ></Container> // __EXCLUDE__
 ) // __EXCLUDE__
